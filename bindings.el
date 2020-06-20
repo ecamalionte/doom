a273a9f66db07286a02b7bc8bf704afed8b53a3c
Eric Camalionte <eric@brainn.co>
2020-06-20 19:57:35 -0300

    Add initial config

---
+++ bindings.el
@@ -0,0 +1,36 @@
+;;; ~/.doom.d/bindings.el -*- lexical-binding: t; -*-
+
+;; Remap alchemist mix test at point
+(map! :leader
+      :nv "t t" nil
+      :desc "Mix test at point"
+      :n "t t" 'alchemist-mix-test-at-point)
+
+;; Remap alchemist mix test file
+(map! :leader
+      :nv "t f" nil
+      :desc "Mix test file"
+      :n "t f" 'alchemist-mix-test-file)
+
+;; Remap alchemist mix test all
+(map! :leader
+      :nv "t a" nil
+      :desc "Mix test all"
+      :n "t a" 'alchemist-mix-test)
+
+;; Remap alchemist create file and test
+(map! :leader
+      :nv "t c c" nil
+      :desc "Create file and test in other window"
+      :n "t c c" #'alchemist-project-toggle-file-and-tests-other-window)
+
+;; Remap opening the sidebar
+(map! :leader
+      :nv "o n" nil
+      :desc "Open treemacs pane"
+      :n "o n" #'+treemacs/toggle)
+;; Remap finding stuff
+(map! :leader
+      :nv "o N" nil
+      :desc "Treemacs find file"
+      :n "o N" 'treemacs-find-file)
